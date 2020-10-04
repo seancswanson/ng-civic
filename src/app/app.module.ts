7e7a13481d645984eb69a9c0a36a371de9593c55
Sean Swanson <sean-swan@hotmail.com>
2020-10-04 11:10:16 -0700

    Removed boilerplate NG code and replaced it with basic styled components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
